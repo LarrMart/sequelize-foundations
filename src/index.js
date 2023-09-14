5457d40446a0358a77f1bc3510d95febaa5f3c5e
Guillermo Larroca Martínez <larrocaguillermo@gmail.com>
2023-09-14 14:52:05 -0300

    Tutorial de sequelize iniciado

--- src/index.js
+++ src/index.js
@@ -1,32 +1,59 @@
-// How to connect to a local database.
+require('dotenv').config();
 
-require('dotenv').config()
-const Sequelize = require('Sequelize');
+const dbConnectionData = {
+  host     : process.env.HOST,
+  port     : process.env.PORT,
+  username : process.env.DB_USER,
+  password : process.env.PASSWORD,
+  database : process.env.DB_NAME,
+  dialect  : process.env.DIALECT
+}
 
-const sequelize = new Sequelize({
-	host     : process.env.HOST,
-	port     : process.env.PORT,
-	username : process.env.DB_USER,
-	password : process.env.PASSWORD,
-	database : process.env.DB_NAME,
-	dialect  : 'mysql'
-});
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = new Sequelize(dbConnectionData);
 
-const database = () => {
-	console.log(new Date())
-	sequelize.authenticate()
-		.then(() => {
-			console.log('Connection succesful!');
-			console.log(new Date())
-		}).finally(() => {
-			sequelize.close()
-			console.log('Connection close!');
-			console.log(new Date())
-		})
-	
-	
-};
+const Person = sequelize.define('Person', {
+  // Model attributes are defined here
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  lastName: {
+    type: DataTypes.STRING
+    // allowNull defaults to true
+  }
+}, {
+  // Other model options go here
+  // freezeTableName: true
+  tableName: 'People',
+  timestamps: false
+})
 
-database();
+async function main() {
+  try {
+  	await Person.sync({force: true});
+  	await Person.bulkCreate(
+      [
+        {
+          firstName : 'Guillermo',
+          lastName  : 'Larroca'
+        },
+        {
+          firstName : 'Barbara',
+          lastName : 'Doe'
+        },
+        {
+          firstName: 'Sofi',
+          lastName: 'Jujuy'
+        }
+      ]
+    );
+	  const result = await Person.findAll();
+    console.log(result.map(elem => elem.dataValues));
+  
+  } catch(error) {
+    console.log(error);
+  }
+}
 
-
+main();
